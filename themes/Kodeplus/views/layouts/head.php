604e756f8ddf36ab9a5fe7a8b3f37ab7d9191cb7
Luan To <luantt1111@gmail.com>
2016-03-23 17:09:48 +0700

    #19 - Move space menu to top menu

--- themes/Kodeplus/views/layouts/head.php
+++ themes/Kodeplus/views/layouts/head.php
@@ -1,3 +1,9 @@
 <link href="<?php echo $this->theme->getBaseUrl() . '/css/theme.css'; ?>" rel="stylesheet">
+<link href="<?php echo $this->theme->getBaseUrl() . '/css/kodeplusTheme.css'; ?>" rel="stylesheet">
 <link href="<?php echo $this->theme->getBaseUrl() . '/css/spaceChooser.css'; ?>" rel="stylesheet">
-<link href="<?php echo $this->theme->getBaseUrl() . '/font/open_sans/open-sans.css'; ?>" rel="stylesheet">
+
+<link href="<?php echo $this->theme->getBaseUrl() . '/css/normalize.css'; ?>" rel="stylesheet">
+<link href="<?php echo $this->theme->getBaseUrl() . '/css/tabs.css'; ?>" rel="stylesheet">
+<link href="<?php echo $this->theme->getBaseUrl() . '/css/tabstyles.css'; ?>" rel="stylesheet">
+
+<script src="<?php echo $this->theme->getBaseUrl() . '/js/modernizr.custom.js'; ?>" type="text/javascript"></script>
