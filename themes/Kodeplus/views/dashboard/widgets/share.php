01ac31abddbe3f74295daf0206404a463a43c1df
khoadp <dkhoa81@gmail.com>
2016-04-21 11:00:31 +0700

    Fixed some typos

--- themes/Kodeplus/views/dashboard/widgets/share.php
+++ themes/Kodeplus/views/dashboard/widgets/share.php
@@ -12,29 +12,29 @@ use yii\helpers\Url;
     </div>
     <div class="panel-body">
 
-        <p>We are glad that you are using <a href="http://yocommunity.example.com" target="_blank" class="colorInfo">Yo Community</a>. If you like it, please support the project and share Yocommunity with your friends and colleagues.</p>
+        <p>We are glad that you are using <a href="http://yocommunity.com" target="_blank" class="colorInfo">Yo Community</a>. If you like it, please support the project and share Yocommunity with your friends and colleagues.</p>
 
         <a class="popup"
-           href="http://twitter.com/intent/tweet?status=I'm really impressed by @Kodeplus. An Open Source Social Network for team communication and collaboration. I love it! http://yocommunity.example.com">
+           href="http://twitter.com/intent/tweet?status=I'm really impressed by @Kodeplus. An Open Source Social Network for team communication and collaboration. I love it! http://yocommunity.com">
             <div class="share-icon share-icon-twitter"><i class="fa fa-twitter"></i></div>
             <div class="share-text"><?php echo Yii::t('DashboardModule.widgets_views_share', 'Tweet about Yo Community'); ?></div>
         </a>
         <!--<script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'http';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>-->
         <div class="clearfix"></div>
-        <a class="popup" href="http://www.facebook.com/share.php?u=http://yocommunity.example.com">
+        <a class="popup" href="http://www.facebook.com/share.php?u=http://yocommunity.com">
             <div class="share-icon share-icon-facebook"><i class="fa fa-facebook"></i></div>
             <div class="share-text"><?php echo Yii::t('DashboardModule.widgets_views_share', 'Post a message on Facebook'); ?></div>
         </a>
 
         <div class="clearfix"></div>
-        <a class="popup" href="http://plus.google.com/share?url=http://yocommunity.example.com">
+        <a class="popup" href="http://plus.google.com/share?url=http://yocommunity.com">
             <div class="share-icon share-icon-google-plus"><i class="fa fa-google-plus"></i></div>
             <div class="share-text"><?php echo Yii::t('DashboardModule.widgets_views_share', 'Share on Google+'); ?></div>
         </a>
 
         <div class="clearfix"></div>
         <a class="popup"
-           href="http://www.linkedin.com/shareArticle?mini=true&url=http://yocommunity.example.com&title=Yo Community - The flexible Open Source Social Network Kit for Collaboration">
+           href="http://www.linkedin.com/shareArticle?mini=true&url=http://yocommunity.com&title=Yo Community - The flexible Open Source Social Network Kit for Collaboration">
             <div class="share-icon share-icon-linkedin"><i class="fa fa-linkedin-square"></i></div>
             <div class="share-text"><?php echo Yii::t('DashboardModule.widgets_views_share', 'Share with people on LinkedIn '); ?></div>
         </a>
