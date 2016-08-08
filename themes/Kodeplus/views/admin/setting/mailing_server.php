22c7d67140519a61b2f0d398c933e19db227f102
InvisK <iminvisk@gmail.com>
2016-08-08 15:05:43 +0700

    #56 - conflict removed

--- themes/Kodeplus/views/admin/setting/mailing_server.php
+++ themes/Kodeplus/views/admin/setting/mailing_server.php
@@ -27,70 +27,63 @@ use yii\helpers\Url;
 
         <?php echo $form->errorSummary($model); ?>
 
+    <div class="form-group">
+        <?php echo $form->labelEx($model, 'systemEmailName'); ?>
+        <?php echo $form->textField($model, 'systemEmailName', array('class' => 'form-control', 'readonly' => Setting::IsFixed('mailer.systemEmailName'))); ?>
+    </div>
+
+
+    <div class="form-group">
+        <?php echo $form->labelEx($model, 'transportType'); ?>
+        <?php echo $form->dropDownList($model, 'transportType', $transportTypes, array('class' => 'form-control', 'readonly' => Setting::IsFixed('mailer.transportType'))); ?>
+    </div>
+
+    <div id="smtpOptions">
+        <hr>
+        <h4> <?php echo Yii::t('AdminModule.views_setting_mailing_server', 'SMTP Options'); ?> </h4>
+
         <div class="form-group">
-            <?php echo $form->labelEx($model, 'systemEmailAddress'); ?>
-            <?php echo $form->textField($model, 'systemEmailAddress', array('class' => 'form-control', 'readonly' => Setting::IsFixed('systemEmailAddress', 'mailing'))); ?>
+            <?php echo $form->labelEx($model, 'hostname'); ?>
+            <?php echo $form->textField($model, 'hostname', array('class' => 'form-control', 'readonly' => Setting::IsFixed('mailer.hostname'))); ?>
         </div>
 
-
         <div class="form-group">
-            <?php echo $form->labelEx($model, 'systemEmailName'); ?>
-            <?php echo $form->textField($model, 'systemEmailName', array('class' => 'form-control', 'readonly' => Setting::IsFixed('systemEmailName', 'mailing'))); ?>
+            <?php echo $form->labelEx($model, 'username'); ?>
+            <?php echo $form->textField($model, 'username', array('class' => 'form-control', 'readonly' => Setting::IsFixed('mailer.username'))); ?>
         </div>
 
-
         <div class="form-group">
-            <?php echo $form->labelEx($model, 'transportType'); ?>
-            <?php echo $form->dropDownList($model, 'transportType', $transportTypes, array('class' => 'form-control', 'readonly' => Setting::IsFixed('transportType', 'mailing'))); ?>
+            <?php echo $form->labelEx($model, 'password'); ?>
+            <?php echo $form->passwordField($model, 'password', array('class' => 'form-control', 'readonly' => Setting::IsFixed('mailer.password'))); ?>
         </div>
 
-        <div id="smtpOptions">
-            <hr>
-            <h4> <?php echo Yii::t('AdminModule.views_setting_mailing_server', 'SMTP Options'); ?> </h4>
+        <div class="form-group">
+            <?php echo $form->labelEx($model, 'port'); ?>
+            <?php echo $form->textField($model, 'port', array('class' => 'form-control', 'readonly' => Setting::IsFixed('mailer.port'))); ?>
+        </div>
 
+        <div class="form-group">
+            <?php echo $form->labelEx($model, 'encryption'); ?>
+            <?php echo $form->dropDownList($model, 'encryption', $encryptionTypes, array('class' => 'form-control', 'readonly' => Setting::IsFixed('mailer.encryption'))); ?>
+        </div>
+
+        <div id="encryptionOptions">
             <div class="form-group">
-                <?php echo $form->labelEx($model, 'hostname'); ?>
-                <?php echo $form->textField($model, 'hostname', array('class' => 'form-control', 'readonly' => Setting::IsFixed('hostname', 'mailing'))); ?>
-            </div>
-
-            <div class="form-group">
-                <?php echo $form->labelEx($model, 'username'); ?>
-                <?php echo $form->textField($model, 'username', array('class' => 'form-control', 'readonly' => Setting::IsFixed('username', 'mailing'))); ?>
-            </div>
-
-            <div class="form-group">
-                <?php echo $form->labelEx($model, 'password'); ?>
-                <?php echo $form->passwordField($model, 'password', array('class' => 'form-control', 'readonly' => Setting::IsFixed('password', 'mailing'))); ?>
-            </div>
-
-            <div class="form-group">
-                <?php echo $form->labelEx($model, 'port'); ?>
-                <?php echo $form->textField($model, 'port', array('class' => 'form-control', 'readonly' => Setting::IsFixed('port', 'mailing'))); ?>
-            </div>
-
-            <div class="form-group">
-                <?php echo $form->labelEx($model, 'encryption'); ?>
-                <?php echo $form->dropDownList($model, 'encryption', $encryptionTypes, array('class' => 'form-control', 'readonly' => Setting::IsFixed('encryption', 'mailing'))); ?>
-            </div>
-
-            <div id="encryptionOptions">
-                <div class="form-group">
-                    <strong>Encryption Options</strong>
-                    <div class="checkbox">
-                        <label>
-                            <?php echo $form->checkbox($model, 'allowSelfSignedCerts', array('class' => 'form-control', 'readonly' => Setting::IsFixed('allowSelfSignedCerts', 'mailing'))); ?>
-                            <?php echo $model->getAttributeLabel('allowSelfSignedCerts'); ?>
-                        </label>
-                    </div>
+                <strong>Encryption Options</strong>
+                <div class="checkbox">
+                    <label>
+                        <?php echo $form->checkbox($model, 'allowSelfSignedCerts', array('class' => 'form-control', 'readonly' => Setting::IsFixed('mailer.allowSelfSignedCerts'))); ?>
+                        <?php echo $model->getAttributeLabel('allowSelfSignedCerts'); ?>
+                    </label>
                 </div>
             </div>
         </div>
+    </div>
         <hr>
         <?php echo CHtml::submitButton(Yii::t('AdminModule.views_setting_mailing_server', 'Save'), array('class' => 'btn btn-primary')); ?>
 
         <?php echo \humhub\widgets\DataSaved::widget(); ?>
         <?php CActiveForm::end(); ?>
-
     </div>
 </div>
 
